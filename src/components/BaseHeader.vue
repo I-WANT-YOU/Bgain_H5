43e0bb9dd41b81bbb697757858165ac09d7236a0
houshiduo <shiduo.hou@fin-bee.com>
2019-07-26 23:09:07 +0800

    feat(BaseHeader): 添加 Product 通用 Header

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,5 +1,12 @@
 <template>
-  <svg-icon icon-class="bgain"></svg-icon>
+  <div class="header">
+    <svg-icon icon-class="bgain" class="header__bgain"/>
+    <span class="header__title">定期</span>
+    <div class="header__right">
+      <svg-icon icon-class="user" class="header__user" />
+      <svg-icon icon-class="more" class="header__more" />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +15,42 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  .header {
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px 22px;
+    font-size: 14px;
 
+    .header__bgain {
+      width: 67px;
+      height: 18px;
+    }
+
+    .header__title {
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+      font-size: 14px;
+      color: #0F3256;
+      letter-spacing: 0;
+      line-height: 37.36px;
+    }
+
+    .header__right {
+
+      .header__user {
+        width: 17px;
+        height: 16px;
+        margin-right: 18px;
+      }
+
+      .header__more {
+        width: 16px;
+        height: 14px;
+      }
+    }
+  }
 </style>
