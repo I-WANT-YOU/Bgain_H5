dd1b0886f51fbbe612a9d6b40f3ec6d71d8b684f
ansonpro <houshiduo@gmail.com>
2019-08-01 11:50:40 +0800

    feat(Fund): 基金 Api 及 Actions 接入

--- src/api/product/fund.js
+++ src/api/product/fund.js
@@ -16,6 +16,50 @@ class FundService {
       throw new Error(error);
     }
   }
+
+  static async getFundProductDetail(id) {
+    try {
+      const requestOptions = {
+        url: '/fund/fund-details',
+        headers: { 'Content-Type': 'application/json' },
+        method: 'post',
+        data: {
+          product_id: id,
+        },
+      };
+      const response = await request(requestOptions);
+      return handlerResponse(response);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  static async getFundBuyInfo(id) {
+    try {
+      const requestOptions = {
+        url: `/fund-product-order/get-fund-buy-info/${id}`,
+        headers: { 'Content-Type': 'application/json' },
+        method: 'get',
+      };
+      const response = await request(requestOptions);
+      return handlerResponse(response);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  static async getHoldingFunds() {
+    try {
+      const requestOptions = {
+        url: '/fund/owner-fund',
+        method: 'get',
+      };
+      const response = await request(requestOptions);
+      return handlerResponse(response);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 }
 
 export default FundService;
--- src/store/modules/product/fund.js
+++ src/store/modules/product/fund.js
@@ -8,11 +8,16 @@ import { FUND_STATUS } from '@/constants/options';
 const state = {
   funds: [],
   currencies: [],
+  fund: {},
+  fundBuyInfo: {},
+  holdingFunds: {},
 };
 
 const getters = {
   initialFunds: state => state.funds.filter(({ status }) => status === FUND_STATUS.INITIAL),
   otherFunds: state => state.funds.filter(({ status }) => status !== FUND_STATUS.INITIAL),
+  fundDetail: state => get(state.fund, 'fund_product_detail_record', {}),
+  fundNavHistories: state => get(state.fund, 'fund_nav_histories', []),
 };
 
 const mutations = {
@@ -22,9 +27,19 @@ const mutations = {
   [types.GET_FUND_CURRENCIES](state, payload) {
     state.currencies = payload;
   },
+  [types.GET_FUND_PRODUCT_DETAIL](state, payload) {
+    state.fund = payload;
+  },
+  [types.GET_FUND_BUY_INFO](state, payload) {
+    state.fundBuyInfo = payload;
+  },
+  [types.GET_HOLDING_FUNDS](state, payload) {
+    state.holdingFunds = payload;
+  },
 };
 
 const actions = {
+  // 获取基金产品列表
   async getFundProducts({ commit }, { currency, type, risk }) {
     try {
       const response = await FundService.getFundProducts(filterUndefined({
@@ -35,6 +50,39 @@ const actions = {
       const data = await Auth.handlerSuccessResponse(response);
       commit(types.GET_FUND_CURRENCIES, get(data, 'coins', []));
       commit(types.GET_FUND_PRODUCTS, get(data, 'fund_products', []));
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  // 获取基金产品详情
+  async getFundProductDetail({ commit }, productId) {
+    try {
+      const response = await FundService.getFundProductDetail(productId);
+      const data = await Auth.handlerSuccessResponse(response);
+      commit(types.GET_FUND_PRODUCT_DETAIL, data);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  // 获取基金购买页信息
+  async getFundBuyInfo({ commit }, productId) {
+    try {
+      const response = await FundService.getFundBuyInfo(productId);
+      const data = await Auth.handlerSuccessResponse(response);
+      commit(types.GET_FUND_BUY_INFO, data);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  // 获取用户在持基金
+  async getHoldingFunds({ commit }) {
+    try {
+      const response = await FundService.getHoldingFunds();
+      const data = await Auth.handlerSuccessResponse(response);
+      commit(types.GET_HOLDING_FUNDS, data);
     } catch (error) {
       throw error;
     }
