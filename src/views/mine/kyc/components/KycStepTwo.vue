c2c7794c44d6679ccdc9ef947646d6c482504cf1
ansonpro <houshiduo@gmail.com>
2019-08-08 12:17:01 +0800

    feat(KYC): 添加上传图片功能

--- src/views/mine/kyc/components/KycStepTwo.vue
+++ src/views/mine/kyc/components/KycStepTwo.vue
@@ -15,6 +15,44 @@
         ></kyc-field>
       </cell-group>
     </div>
+    <div class="kyc__photos">
+      <div class="photos__titles">
+        <div class="photos__title">证件照片</div>
+        <div class="photos__subtitle">请添加本人清晰的证件照片（不超过10M，jpg、bmp或png）</div>
+      </div>
+      <div class="photos__content">
+        <div
+          class="photo__container"
+          v-visible="uploader.icon !== ''"
+          v-for="(uploader, index) in uploaders[documentType]"
+          :key="uploader.icon">
+          <Uploader
+            :after-read="afterRead"
+            :name="index"
+          >
+            <template v-slot:default>
+              <div class="photo__wrapper">
+                <svg-icon :icon-class="uploader.icon" class="icon-bg-place"/>
+                <svg-icon icon-class="camera" class="icon-camera"/>
+                <van-image
+                  v-if="files[index] !== ''"
+                  class="preview-image"
+                  width="82"
+                  height="52"
+                  fit="contain"
+                  :src="files[index]"
+                >
+                  <template v-slot:loading>
+                    <Loading type="spinner" size="20"/>
+                  </template>
+                </van-image>
+              </div>
+            </template>
+          </Uploader>
+          <div class="photo__label">{{uploader.label}}</div>
+        </div>
+      </div>
+    </div>
     <div class="kyc__button-wrap">
       <bgain-button
         type="info"
@@ -31,6 +69,14 @@
       >
         下一步
       </bgain-button>
+      <bgain-button
+        type="info"
+        :fluid="true"
+        @click="onGetSecretClick"
+        :style="{marginTop: '30px'}"
+      >
+        获取秦健大佬的上传信息
+      </bgain-button>
     </div>
     <Popup v-model="showPicker" position="bottom">
       <Picker
@@ -45,9 +91,12 @@
 </template>
 
 <script>
+import axios from 'axios';
 import {
-  CellGroup, Popup, Picker, Toast,
+  CellGroup, Popup, Picker, Toast, Uploader, Image, Loading,
 } from 'vant';
+import { isEmpty } from 'lodash';
+import { mapState, mapActions } from 'vuex';
 import { checkDocumentNumber } from '@utils/tools';
 import { DOCUMENT_TYPE } from '@/constants/options';
 import BgainButton from '@/components/BgainButton.vue';
@@ -61,6 +110,9 @@ export default {
     CellGroup,
     Popup,
     Picker,
+    Uploader,
+    VanImage: Image,
+    Loading,
   },
   props: {
     documentType: {
@@ -75,9 +127,58 @@ export default {
   data() {
     return {
       showPicker: false,
+      policy: {},
+      files: ['', '', ''],
+      uploaders: {
+        ID: [
+          {
+            icon: 'id-front',
+            label: '身份证正面照',
+          },
+          {
+            icon: 'id-reverse',
+            label: '身份证反面照',
+          },
+          {
+            icon: 'id-person',
+            label: '本人手持证件照',
+          },
+        ],
+        PASSPORT: [
+          {
+            icon: 'id-front',
+            label: '证件照',
+          },
+          {
+            icon: 'id-person',
+            label: '本人手持证件照',
+          },
+          {
+            icon: '',
+            label: '',
+          },
+        ],
+        DRIVER_LICENSE: [
+          {
+            icon: 'id-front',
+            label: '证件照',
+          },
+          {
+            icon: 'id-person',
+            label: '本人手持证件照',
+          },
+          {
+            icon: '',
+            label: '',
+          },
+        ],
+      },
     };
   },
   computed: {
+    ...mapState('app', [
+      'uploadPolicy',
+    ]),
     type() {
       return DOCUMENT_TYPE.filter(({ value }) => value === this.documentType)[0].label;
     },
@@ -86,6 +187,7 @@ export default {
     },
   },
   methods: {
+    ...mapActions('app', ['getUploadPolicy']),
     onPrevClick() {
       this.$emit('change-step', 1);
     },
@@ -109,6 +211,78 @@ export default {
     onDocNumberInput(number) {
       this.$emit('change-doc-number', number);
     },
+    getRandomFileName() {
+      return Math.random()
+        .toString(36)
+        .substring(2, 15) + Math.random()
+        .toString(36)
+        .substring(2, 15);
+    },
+    getSuffix(filename) {
+      const index = filename.lastIndexOf('.');
+      if (index !== -1) {
+        return filename.substring(index);
+      }
+      return '';
+    },
+    async fetchUploadPolicy() {
+      try {
+        await this.getUploadPolicy();
+      } catch (error) {
+        Toast(error);
+      }
+    },
+    // eslint-disable-next-line consistent-return
+    async getPolicy() {
+      const now = Date.parse(new Date()) / 1000;
+      if (isEmpty(this.uploadPolicy)) {
+        return this.fetchUploadPolicy();
+      }
+      if (parseInt(this.uploadPolicy.expire, 10) < now + 3) {
+        return this.fetchUploadPolicy();
+      }
+    },
+    upload(file, index) {
+      const {
+        policy, accessid, signature, dir, host,
+      } = this.uploadPolicy;
+      const photo = file.file;
+      const name = `${this.getRandomFileName()}${this.getSuffix(photo.name)}`;
+      const key = `${dir}${name}`;
+
+      const param = new FormData();
+      param.append('name', `${name}`);
+      param.append('key', `${key}`);
+      param.append('policy', `${policy}`);
+      param.append('OSSAccessKeyId', `${accessid}`);
+      param.append('success_action_status', '200');
+      param.append('signature', `${signature}`);
+      param.append('file', photo, name);
+      axios.post(host, param)
+        .then(() => {
+          this.files.splice(index, 1, `${host}/${key}`);
+          Toast.clear();
+        })
+        .catch((error) => {
+          Toast(error);
+        });
+    },
+    async afterRead(file, { name: index }) {
+      try {
+        Toast.loading({
+          duration: 0,
+          forbidClick: true,
+          message: '上传中...',
+        });
+        await this.getPolicy();
+        this.upload(file, index);
+      } catch (error) {
+        Toast(error);
+      }
+    },
+    onGetSecretClick() {
+      this.getPolicy();
+    },
   },
 };
 </script>
@@ -117,5 +291,69 @@ export default {
   .kyc__document--type {
     font-size: 15px;
     color: #0F3256;
+  }
+
+  .kyc__photos {
+    margin-top: 10px;
+    padding: 16px 0;
+    background: #fff;
+
+    .photos__titles {
+      padding-left: 30px;
+
+      .photos__title {
+        font-size: 14px;
+        color: #6A707D;
+        line-height: 20px;
+        margin-bottom: 3px;
+      }
+
+      .photos__subtitle {
+        font-size: 12px;
+        color: #A8AEB9;
+        line-height: 17px;
+      }
+    }
+
+    .photos__content {
+      margin-top: 15px;
+      padding: 0 30px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      .photo__wrapper {
+        position: relative;
+
+        .icon-bg-place {
+          width: 98px;
+          height: 70px;
+        }
+
+        .icon-camera {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          width: 24px;
+          height: 24px;
+        }
+
+        .preview-image {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+        }
+      }
+
+      .photo__label {
+        margin-top: 10px;
+        font-size: 12px;
+        color: #0F3256;
+        line-height: 17px;
+        text-align: center;
+      }
+    }
   }
 </style>
